48c9122b31b2f402893b51595a3e2a85a789a27c
ohgihara <schu22to0716@icloud.com>
2021-10-09 18:11:22 +0900

    pythonの学習

---
+++ python練習.py
@@ -0,0 +1,25 @@
+print('Hello Python')
+
+print('hello world!')
+
+numbers = [
+  1, 2,
+  3, 4
+]
+print(numbers)
+
+# 条件式の練習
+x = 10
+y = 5
+if x > y:
+  print('xはyより大きい')
+elif x == y:
+  print('xとyは等しい')
+else:
+  print('xはyより小さい')
+
+# 型の判定について
+print(type(123))
+print(type(123.4))
+print(type([1,2,3]))
+
---
+++ フロー制御.py
@@ -0,0 +1,11 @@
+# if分の練習
+x = 10
+y = 5
+if x >= 10 and y >= 10:
+  print('x,yともに10以上です。')
+elif x >= 10 or y >= 10:
+  print('xまたはyが10以上です')
+else:
+  print('xもyも10未満です。')
+
+
