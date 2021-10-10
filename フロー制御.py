3a27a8c2841c75df108a246ec7c765ed2dd2f4c3
ohgihara <schu22to0716@icloud.com>
2021-10-10 18:00:30 +0900

    フロー制御、文字列の練習

--- フロー制御.py
+++ フロー制御.py
@@ -9,3 +9,47 @@ else:
   print('xもyも10未満です。')
 
 
+# whileの練習
+i = 1
+while i < 5:
+  print(i)
+  i += 1
+
+
+i = 3
+while i:
+  print(i)
+  i -= 1
+
+for i in range(1, 5):
+ print(i)
+
+
+# for関数、range関数の練習
+for i in range(3):
+  print(i)
+
+for i in range(10, -10, -5):
+  print(i)
+
+
+
+# break文によるループの終了
+num = 1
+for i in range(1, 10):
+  num *= 2
+  print(i, num)
+  if num > 20:
+    break
+
+
+# continueによるループスキップの練習
+for i in range(1, 10):
+  if i % 3 == 0 or i % 5 == 0:
+    continue
+  print(i)
+
+# 多重ループ
+for i in range(1, 4):
+  for j in range(1, 3):
+    print(i, j)
---
+++ 文字列の理解.py
@@ -0,0 +1,29 @@
+print('今日は')
+
+msg = "Hello! I'm fine."
+print(msg)
+
+print("")
+
+
+print('''今日は
+お元気ですか''')
+
+
+print('Hello\nPython\t!!!')
+
+# 文字列のslice
+msg = 'Hello'
+print(msg[1:3])
+
+# フォーマット済み文字列リテラル
+name = 'Bob'
+age = 25
+print(f'{name}は{age}歳です')
+
+
+
+print('C:\\Users\\ntaka\\hoge.py')
+
+# raw関数を用いているため、エスケープシーケンスが適用されない。
+print(r'C:\Users\ntaka\hoge.py')
